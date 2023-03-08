725b917bc0138887d053e30f4b23ad81835ee983
liuyixiao <liuyixiao050@chinasoftinc.com>
2023-03-08 16:38:57 +0800

    first commit

---
+++ src/main/java/com/wx/controller/WxController.java
@@ -0,0 +1,24 @@
+package com.wx.controller;
+
+import com.soecode.wxtools.api.IService;
+import com.soecode.wxtools.api.WxService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wx")
+public class WxController {
+
+    private IService iService = new WxService();
+
+    @GetMapping
+    public String check(String signature, String timestamp, String nonce, String echostr) {
+        if (iService.checkSignature(signature, timestamp, nonce, echostr)) {
+            return echostr;
+        }
+        return null;
+    }
+
+
+}
