fa3a21d50ce648e278995aa47d28be2a0b024b1d
wushijie <1299756312@qq.com>
2023-03-08 17:47:02 +0800

    add wxVerify.txt

--- src/main/java/com/wx/controller/WxController.java
+++ src/main/java/com/wx/controller/WxController.java
@@ -2,9 +2,17 @@ package com.wx.controller;
 
 import com.soecode.wxtools.api.IService;
 import com.soecode.wxtools.api.WxService;
+import com.soecode.wxtools.bean.WxXmlMessage;
+import com.soecode.wxtools.util.xml.XStreamTransformer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @RestController
 @RequestMapping("/wx")
@@ -19,6 +27,24 @@ public class WxController {
         }
         return null;
     }
+    @PostMapping
+    public void handle(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+        try {
+            // 微信服务器推送过来的是XML格式。
+            WxXmlMessage wx = XStreamTransformer.fromXml(WxXmlMessage.class, request.getInputStream());
+            System.out.println("消息：\n " + wx.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            out.close();
+        }
+
+    }
+
 
 
 }
